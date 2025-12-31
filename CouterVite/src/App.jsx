4aef86f9407e0e5adc554bdc8b6655430f23eddf
pratiksinha20 <pratiksinha198@gmail.com>
2025-12-31 23:51:02 +0530

    2nd modification

--- CouterVite/src/App.jsx
+++ CouterVite/src/App.jsx
@@ -20,13 +20,30 @@ const colors=["white",
   return (
     <>
     <div className="App"
-    style={{backgroundColor: colors[count % colors.length]}}>
+      style={{
+        backgroundColor: colors[count],
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "20px",
+        transition: "background-color 0.3s ease"
+      }}
+      >
       <button className='count'>
         
         count is {count}
         </button>
-        <br />
-        <button
+         <input
+        type="range"
+        min="0"
+        max="10"
+        value={count}
+        onChange={(e) => setCount(Number(e.target.value))}
+      />
+<button
+// disabled={count===0}
   className="plus"
   onClick={() =>
     setCount(prev => {
@@ -41,6 +58,7 @@ const colors=["white",
 </button>
 
 <button
+// disabled={count=== colors.length-1}
   className="minus"
   onClick={() =>
     setCount(prev => {
